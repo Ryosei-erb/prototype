d595048acc1d9357cec471fa5c25f4a448450779
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-03-03 08:09:13 +0900

    modify the product#show view

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :taxons
 
   has_attached_file :image,
-  styles: { medium: "300×300>"}, path: "#{Rails.root}/app/assets/images/:filename"
-  validates_attachment_content_type :image, content_type: [ "image/jpeg", "image/gif", "image/png"]
+  styles: { product: "680×680>", large: "1200×1200>" }, path: "#{Rails.root}/app/assets/images/:filename"
+  validates_attachment_content_type :image, content_type: [ "image/jpeg", "image/gif", "image/png", "image/jpg"]
   validates_attachment_presence :image
 end
