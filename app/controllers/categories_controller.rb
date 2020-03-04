ae55fec8c56200010c80d7bf7fb4bac5235e2eb0
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-03-04 09:17:59 +0900

    Add the categories_index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @taxon = Taxon.find(params[:id])
+    @products = @taxon.products
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,13 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fill: [1200, 1200, "center"]
+  version :product do
+    process resize_to_limit: [300, 300]
+  end
   version :large do
     process resize_to_limit: [500, 500]
   end
+
   if Rails.env.production?
     storage :fog
   else
