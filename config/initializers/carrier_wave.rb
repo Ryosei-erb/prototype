0b18e19a474214930177b7d63b4e7569579126d7
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-03-14 06:18:18 +0900

    Change config/

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,7 +8,6 @@ if Rails.env.production?
 
     config.fog_credentials = {
       # Amazon S3用の設定
-      config.storage = :fog
       :provider              => 'AWS',
       :region                => ENV['S3_REGION'],
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
