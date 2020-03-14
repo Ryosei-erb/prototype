8b9fc72219d30de23343b1e4e380cb27fb52304b
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-03-14 14:00:39 +0900

    Change set geocoder

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,7 +1,7 @@
 class Map < ApplicationRecord
   belongs_to :product
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :address_changed?
   acts_as_mappable(default_units: :kms,
                   default_formula: :sphere,
                   distance_field_name: :distance,
